e7b3ff1b998cbc9dfae58eaec372b9feab8dd94d
Anthony <ajtalvarez@gmail.com>
2017-12-04 22:05:42 -0500

    Created delete user functionality

--- controllers/session.js
+++ controllers/session.js
@@ -3,6 +3,11 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const User = require('../models/users.js');
+const methodOverride = require('method-override');
+
+// Middleware
+
+router.use(methodOverride('_method'));
 
 // Routes
 
@@ -65,12 +70,42 @@ router.post('/register', async (req, res, next) => {
   }
 });
 
+// Logout/Destroy Session
 router.get('/logout', (req, res) => {
   req.session.destroy();
   console.log(req.session);
   res.redirect('/user/login');
 });
 
+// Test
+router.get('/test', (req, res) => {
+  res.send(req.session);
+})
+
+// Settings Page
+router.get('/:id/settings', async (req, res) => {
+  const userToEdit = await User.findOne({username: req.params.id});
+  console.log(userToEdit);
+  res.render(
+    'user/edit.ejs',
+    {
+      user: userToEdit,
+      index: req.params.id
+    }
+  );
+});
+
+// Delete User Route
+router.delete('/:id/', async (req, res) => {
+  console.log(req.params.id);
+  const user = await User.findOne({username: req.params.id});
+  console.log(user);
+  await user.remove();
+  // await Comment.remove({user: user._id});
+  res.redirect('/user/login');
+})
+
+// User Dashboard Route
 router.get('/:id', async (req, res) => {
   if (req.session.logged) {
     res.render('user/show.ejs', {
